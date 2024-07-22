78cc94b4b65d6164340a529d5616ae6375582749
Yen <73730090+Monles@users.noreply.github.com>
2024-07-22 20:39:58 +0100

    ch_03

--- Command_Line_Rust/catr/src/main.rs
+++ Command_Line_Rust/catr/src/main.rs
@@ -1,14 +1,7 @@
-use clap::{Arg, ArgAction, Command};
-use clap::Parser;
 use anyhow::Result;
-
-// #[derive(Debug)] 
-// struct Args { 
-//     files: Vec<String>, 
-//     number_lines: bool, 
-//     number_nonblank_lines: bool, 
-// }
-
+use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
@@ -31,54 +24,47 @@ struct Args {
     number_nonblank_lines: bool,
 }
 
-fn get_args() -> Args {
-    let matches = Command::new("catr")
-        .version("0.1.0")
-        .author("Ken Youens-Clark <kyclark@gmail.com>")
-        .about("Rust version of `cat`")
-        .arg(
-            Arg::new("files")
-                .value_name("FILE")
-                .help("Input file(s)")
-                .num_args(1..)
-                .default_value("-"),
-        )
-        .arg(
-            Arg::new("number")
-                .short('n')
-                .long("number")
-                .help("Number lines")
-                .action(ArgAction::SetTrue)
-                .conflicts_with("number_nonblank"),
-        )
-        .arg(
-            Arg::new("number_nonblank")
-                .short('b')
-                .long("number-nonblank")
-                .help("Number non-blank lines")
-                .action(ArgAction::SetTrue),
-        )
-        .get_matches();
-
-    Args {
-        files: matches.get_many("files").unwrap().cloned().collect(),
-        number_lines: matches.get_flag("number"),
-        number_nonblank_lines: matches.get_flag("number_nonblank"),
+// --------------------------------------------------
+fn main() {
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
     }
 }
 
-
-
+// --------------------------------------------------
 fn run(args: Args) -> Result<()> {
     for filename in args.files {
-        println!("{filename}");
+        match open(&filename) {
+            Err(e) => eprintln!("{filename}: {e}"),
+            Ok(file) => {
+                let mut prev_num = 0;
+                for (line_num, line_result) in file.lines().enumerate() {
+                    let line = line_result?;
+                    if args.number_lines {
+                        println!("{:6}\t{line}", line_num + 1);
+                    } else if args.number_nonblank_lines {
+                        if line.is_empty() {
+                            println!();
+                        } else {
+                            prev_num += 1;
+                            println!("{prev_num:6}\t{line}");
+                        }
+                    } else {
+                        println!("{line}");
+                    }
+                }
+            }
+        }
     }
+
     Ok(())
 }
 
-fn main() {
-    if let Err(e) = run(get_args()) {
-        eprintln!("{e}");
-        std::process::exit(1);
+// --------------------------------------------------
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
     }
 }
